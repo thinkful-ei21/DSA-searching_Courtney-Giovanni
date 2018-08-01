888fe1c3c999fd093d276e04cfda28112b664391
giovanni <mormagli@gmail.com>
2018-08-01 11:15:34 -0500

    wrote binary search algorithm

--- dsa-searching/src/algorithm.js
+++ dsa-searching/src/algorithm.js
@@ -1,3 +1,6 @@
+'use strict';
+
+
 const testArr = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
 
@@ -11,4 +14,30 @@ function linearSearch(arr, value) {
   return `Value was not in list. Algorithm took ${arr.length} tries`;
 }
 
-console.log(linearSearch(testArr, 115));
+// console.log(linearSearch(testArr, 115));
+
+
+testArr.sort((a,b) => a-b );
+
+function binarySearch(arr, value, start = 0, end = arr.length, cycles=1){
+    
+  let index = Math.floor((start+end)/2);
+  console.log(cycles, index, arr[index])
+
+
+  if(arr[index]===value){
+    return `Algorithm took ${cycles} of tries to find value`;
+  }
+  else if( start === end){
+    return `Value was not in list. Algorithm took ${cycles} tries`;
+  }
+  else if(arr[index] > value){
+    return binarySearch(arr, value, start, index -1, cycles +1);
+  }
+  else if(arr[index] < value){
+    return binarySearch(arr, value, index +1, end, cycles +1);
+  }
+}
+
+
+console.log(binarySearch(testArr, 45));
